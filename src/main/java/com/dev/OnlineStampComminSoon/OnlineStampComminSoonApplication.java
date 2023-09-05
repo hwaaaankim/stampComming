c7e0770a97797f672a2c8df4342967bb6c1d7105
thramania <thramania@hanmail.net>
2023-09-05 13:55:18 +0900

    -first commit

---
+++ src/main/java/com/dev/OnlineStampComminSoon/OnlineStampComminSoonApplication.java
@@ -0,0 +1,13 @@
+package com.dev.OnlineStampComminSoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStampComminSoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStampComminSoonApplication.class, args);
+	}
+
+}
